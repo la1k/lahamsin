6f1f9a9a55813846426b0906c6443ae4de24dbde
Per Kristian J Fjellby <perkrifj@samfundet.no>
2014-02-23 18:22:51 +0100

    Initial commit

---
+++ README.mc
@@ -0,0 +1,6 @@
+Simple script to search the 'lahamsin' file (containing info about Norwegian ham radio operators) provided by the Norwegian Post and Telecommunications Authority.
+
+Download 'lahamsin.csv'. Run 'lahamsin.rb' with an argument, e.g.
+'ruby lahamsin.rb la6xta', or create an alias for simpler access.
+
+
---
+++ lahamsin.rb
@@ -0,0 +1,15 @@
+require 'csv'
+
+root = File.expand_path(File.dirname(__FILE__))
+if ! File.exists?(File.join(root, "lahamsin.csv"))
+  puts "Failed to load lahamsin.csv. Place the file in the same folder as the ruby script"
+  exit 1
+end
+
+csv_text = File.read(File.join(root, "lahamsin.csv"), :encoding => 'iso-8859-1')
+csv = CSV.parse(csv_text, :headers => true, :col_sep => ";")
+csv.each do |row|
+  if row.to_s.downcase.include? ARGV[0].to_s.downcase
+    puts "#{row[0]} : #{row[1]} : #{row[2]}"
+  end
+end
